2fed73e74f2c842d2a687bcf19f9a7cd846b8f61
FOXDOSIMPREZA <159257435+FOXDOSIMPREZA@users.noreply.github.com>
2024-02-14 14:27:27 +0300

    Create z2.py

---
+++ z2.py
@@ -0,0 +1,38 @@
+##1
+x = int(input())
+y = int(input())
+if x>y:
+    max=x
+    min=y
+else:
+    max=y
+    min=x
+z = min + 0.5 / (1 + max**2)
+
+print("Значение z равно:",z)
+##2
+x = int(input())
+y = int(input())
+if x>y:
+    max=x
+else:min=y
+if x >=0:
+
+
+    print("2)z=",max)
+else:
+    print("2)z=",min)
+    ##3
+  x = int(input())
+y = int(input())
+if x>y:
+    max=x
+else:min=y
+if x >=0:
+    print("3)z=",min)
+else:
+    if x**2>=y**2:
+        max=x**2
+    else:
+        min= y**2
+    print("3) z =",max)
