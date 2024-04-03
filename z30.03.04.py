dd012119be859e152bcf2f1654d56d2698465368
bananalover <159257435+FOXDOSIMPREZA@users.noreply.github.com>
2024-04-03 13:59:20 +0300

    Create z30.03.04.py

---
+++ z30.03.04.py
@@ -0,0 +1,23 @@
+a=input()
+sum=0
+print("1) цифр"б,len(a))
+for i in a:
+    sum=int(a)
+print("2) сумма цифр",sum)
+if int(a)//100 !=0:
+    print("3) входит")
+else:
+    print("3) не входит")
+res=list(a)[::-1]
+print("4)",''.join(res))
+sifr=lem(a)-1
+listik=list(a)
+first=listik[0]
+listik[0] =listik[cifr]
+listik[cifr]=first
+print("5)",''.join(listik))
+print("6) нулей применено",a.count('0'))
+for k in a:
+    kv+=k**2
+if kv>int(a):
+    print]('7) сумма квадратов больше числа
