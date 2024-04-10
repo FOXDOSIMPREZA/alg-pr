35b2b20128dffa373c0636fdef52a52ae4db932b
bananalover <159257435+FOXDOSIMPREZA@users.noreply.github.com>
2024-04-10 11:52:46 +0300

    Create z29.10.04.py

---
+++ z29.10.04.py
@@ -0,0 +1,12 @@
+b = []
+max = x
+min = x
+while x!=0:
+    b.append(x)
+    if x > max:
+        max=x
+    if x < min:
+        min=x
+    x = int(input())
+print('диапозон:',str(max)+'-'+str(min))
+print('меньшее число',min,'по счету',b.index(min)+1)
